136099855061208e0c1338e1a5b4dbd667c1e76d
Jones Romão <jones@Pr3d4d0r.(none)>
2014-02-24 19:05:25 -0300

    Adicionando novas Models

--- settings.py
+++ settings.py
@@ -106,7 +106,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/jones/mytemplates/admin/base_site.html',
 )
 
 INSTALLED_APPS = (
