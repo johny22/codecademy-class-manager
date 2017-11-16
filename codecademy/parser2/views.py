82176b1497543cd3fc722d6db446a31c1e5114ff
Jones Romão <jones.rom22@gmail.com>
2017-11-15 23:27:44 -0300

    Portando para a versão mais atual do django

--- codecademy/parser2/views.py
+++ codecademy/parser2/views.py
@@ -5,7 +5,7 @@
 from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render_to_response
-from Colect import Collector
+from .Colect import Collector
 from codecademy.parser2.models import *
 
 
@@ -59,78 +59,78 @@ def InserirNoBD(request):
     html = """<hmtl>
                   <body>
 
-                 	<h1>Atualização efetuada com sucesso!</h1>
+                    <h1>Atualização efetuada com sucesso!</h1>
                     </body></html>"""
     return HttpResponse(html)
 
 
 
 def index(request):
-	now = datetime.now()
-	Perfis = Perfil.objects.all()
-	Nomes = ""
-	Pontos = []
-	Pks = []
-	for i in range(len(Perfis)):
-		if(Nomes == ""):
-			Nomes += Perfis[i].nome
-		else:
-			Nomes += "." + Perfis[i].nome
-		Pontos.append(Perfis[i].total_ponts)
-		Pks.append(Perfis[i].pk)
-	return render_to_response('index.html', locals())	
+    now = datetime.now()
+    Perfis = Perfil.objects.all()
+    Nomes = ""
+    Pontos = []
+    Pks = []
+    for i in range(len(Perfis)):
+        if(Nomes == ""):
+            Nomes += Perfis[i].nome
+        else:
+            Nomes += "." + Perfis[i].nome
+        Pontos.append(Perfis[i].total_ponts)
+        Pks.append(Perfis[i].pk)
+    return render_to_response('index.html', locals())
 
-		
-		
-		
-	
+
+
+
+
 
 def charts(request, PK):
-	historico = History.objects.all().order_by('data_extract')
-	datas = ""
-	Pk = 0
-	for i in range(len(historico)):
-		if(historico[i].perfil.pk == int(PK)):
-			data = str(historico[i].data_extract.day) + "/" + str(historico[i].data_extract.month)  + "/" + str(historico[i].data_extract.year)
-			if (Pk != historico[i].pk):
-				if(datas == ""):
-					datas += data
-				else:
-					datas +=  " " + data
-			Pk = historico[i].pk
+    historico = History.objects.all().order_by('data_extract')
+    datas = ""
+    Pk = 0
+    for i in range(len(historico)):
+        if(historico[i].perfil.pk == int(PK)):
+            data = str(historico[i].data_extract.day) + "/" + str(historico[i].data_extract.month)  + "/" + str(historico[i].data_extract.year)
+            if (Pk != historico[i].pk):
+                if(datas == ""):
+                    datas += data
+                else:
+                    datas +=  " " + data
+            Pk = historico[i].pk
 
-	ordered_hist = History.objects.all().order_by('perfil')
-	Pontos = []
-	perf = Perfil.objects.get(pk=int(PK))
-	Nome = perf.nome
-	Melhor = perf.melhorP
+    ordered_hist = History.objects.all().order_by('perfil')
+    Pontos = []
+    perf = Perfil.objects.get(pk=int(PK))
+    Nome = perf.nome
+    Melhor = perf.melhorP
         Hoje = perf.hoje
         Dias_seg = perf.dias_seg
         Total = perf.total_ponts
 
-	for i in range(len(ordered_hist)):
-		if(ordered_hist[i].perfil.pk == int(PK)):
-			Pontos.append(ordered_hist[i].total_ponts)
-	
-	return render_to_response('charts.html', locals())
+    # for i in range(len(ordered_hist)):
+    #     if(ordered_hist[i].perfil.pk == int(PK)):
+    #         Pontos.append(ordered_hist[i].total_ponts)
+
+    return render_to_response('charts.html', locals())
 
 def badges(request, PK):
-	badges = Achievements.objects.all().order_by('perfil')
-	datas = ""
-	nomes = ""
-	imgUrl = ""
+    badges = Achievements.objects.all().order_by('perfil')
+    datas = ""
+    nomes = ""
+    imgUrl = ""
 
-	
-	for i in range(len(badges)):
-		if(badges[i].perfil.pk == int(PK)):
-			data = str(badges[i].data.day) + "/" + str(badges[i].data.month)  + "/" + str(badges[i].data.year)
-			if(datas == ""):
+
+    for i in range(len(badges)):
+        if(badges[i].perfil.pk == int(PK)):
+            data = str(badges[i].data.day) + "/" + str(badges[i].data.month)  + "/" + str(badges[i].data.year)
+            if(datas == ""):
                             datas += data
-			else:
-			    datas +=  " " + data
+            else:
+                datas +=  " " + data
 
-	for i in range(len(badges)):
-		if(badges[i].perfil.pk == int(PK)):
+    for i in range(len(badges)):
+        if(badges[i].perfil.pk == int(PK)):
                     nome = badges[i].nome
                     if(nomes == ""):
                         nomes += nome
@@ -138,7 +138,7 @@ def badges(request, PK):
                         nomes +=  "." + nome
 
         for i in range(len(badges)):
-		if(badges[i].perfil.pk == int(PK)):
+        if(badges[i].perfil.pk == int(PK)):
                     url = badges[i].imgUrl
                     if(imgUrl == ""):
                         imgUrl += url
@@ -146,23 +146,23 @@ def badges(request, PK):
                         imgUrl +=  " " + url
                     
         Nome = Perfil.objects.get(pk=int(PK)).nome
-	qt = len(nomes[:].split("."))
+    qt = len(nomes[:].split("."))
 
-	return render_to_response('badges.html', locals())
+    return render_to_response('badges.html', locals())
 
 def track_percent(request, PK):
-	now = datetime.now()
-	tracks = Track.objects.all()
-	track_names = ""
-	percents = []
-	for i in tracks:
-		if(i.perfil.pk == int(PK)):
-			percents.append(i.percent)
-			if(track_names == ""):
-				track_names += i.nome
-			else:
-				track_names += "." + i.nome
-	return render_to_response('track_percent.html', locals())
+    now = datetime.now()
+    tracks = Track.objects.all()
+    track_names = ""
+    percents = []
+    for i in tracks:
+        if(i.perfil.pk == int(PK)):
+            percents.append(i.percent)
+            if(track_names == ""):
+                track_names += i.nome
+            else:
+                track_names += "." + i.nome
+    return render_to_response('track_percent.html', locals())
 
 
 
--- codecademy/urls.py
+++ codecademy/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from codecademy.parser2.views import *
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'codecademy.views.home', name='home'),
     # url(r'^codecademy/', include('codecademy.foo.urls')),
@@ -13,12 +14,12 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-   url(r'^addDB/$', 'codecademy.parser2.views.InserirNoBD'),
-   url(r'^charts/(?P<PK>\d+)/$', 'codecademy.parser2.views.charts'),
-   url(r'^badges/(?P<PK>\d+)/$', 'codecademy.parser2.views.badges'),
-   url(r'^track_percent/(?P<PK>\d+)/$', 'codecademy.parser2.views.track_percent'),
+   url(r'^addDB/$', InserirNoBD),
+   url(r'^charts/(?P<PK>\d+)/$', charts),
+   url(r'^badges/(?P<PK>\d+)/$', badges),
+   url(r'^track_percent/(?P<PK>\d+)/$', track_percent),
    url(r'^admin/', include(admin.site.urls)),
-   url(r'^$', 'codecademy.parser2.views.index', name='home'),
+   url(r'^$', index, name='home'),
    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': '/home/jones/codecademy/codecademy/static/'}),
-)
+]
