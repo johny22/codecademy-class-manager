9ae9374f26bf117aee36042ab33ab9008249850e
Jones Romão <jones.rom22@gmail.com>
2019-03-13 17:37:12 -0300

    Retirando configurações obsoletas e ajustando urls

--- codecademy/urls.py
+++ codecademy/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('parser2.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('parser2.urls'))
 ]
--- parser2/urls.py
+++ parser2/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import include, url
-
+from django.urls import include, path
 from django.contrib import admin
+
 from .views import *
 
 admin.autodiscover()
 
 urlpatterns = [
-   url(r'^addDB/$', InserirNoBD),
-   url(r'^charts/(?P<PK>\d+)/$', charts),
-   url(r'^badges/(?P<PK>\d+)/$', badges),
-   url(r'^track_percent/(?P<PK>\d+)/$', track_percent),
-   url(r'^admin/', include(admin.site.urls)),
-   url(r'^$', index, name='home')
+   path('addDB/', InserirNoBD),
+   path('charts/<int:pk>/', charts),
+   path('badges/<int:pk>/', badges),
+   path('track_percent/<int:pk>/', track_percent),
+   path('', index, name='home')
 ]
